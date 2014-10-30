70a0a4dda2cc6d38c5794cd614e2f304bd2970cc
marleenberg1990 <marleenberg@251-85-102.wifi-owb.workstations.avans.nl>
2014-10-30 21:52:57 +0100

    ClassCertificates part2

---
+++ ch5/ClassCertificatesPt2/ClassCertificatesPt2/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  ClassCertificatesPt2
+//
+//  Created by Marleen Berg on 30-10-14.
+//  Copyright (c) 2014 marleen berg. All rights reserved.
+//
+
+#include <stdio.h>
+#include <unistd.h>
+void congratulateStudent (char *student, char *course, int numDays){
+    printf("%s has done as much %s programming as I could fit into %d days \n", student, course, numDays);
+}
+int main(int argc, const char * argv[])
+{
+    congratulateStudent("Kate", "Cocoa", 3);
+    sleep(2);
+    congratulateStudent("Bo", "Objective-C", 2);
+    sleep(2);
+    congratulateStudent("Mike", "Python", 5);
+    sleep(2);
+    congratulateStudent("Liz", "iOS", 5);
+
+    return 0;
+}
