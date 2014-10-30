044d1644d2ce13d4cb51c3b41311badd7f9a4ebf
marleenberg1990 <marleenberg@251-85-102.wifi-owb.workstations.avans.nl>
2014-10-30 21:32:16 +0100

    challenge chapter 3

---
+++ ch3/TwoFloats/TwoFloats/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  TwoFloats
+//
+//  Created by Marleen Berg on 30-10-14.
+//  Copyright (c) 2014 marleen berg. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    float float1 = 3.14;
+    float float2 = 42.0;
+    
+    double sumOfFloats = float1 + float2;
+    
+    printf("The sum of float1 and float2 is %f \n", sumOfFloats);
+    return 0;
+}
