16c40fe9678a5d73afd7549b0ea8f306fb1ff786
marleenberg1990 <marleenberg@251-85-102.wifi-owb.workstations.avans.nl>
2014-10-30 22:10:08 +0100

    Turkey cooking time pt2

---
+++ ch5/CookTimeForTurkeyPt2/CookTimeForTurkeyPt2/main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  CookTimeForTurkeyPt2
+//
+//  Created by Marleen Berg on 30-10-14.
+//  Copyright (c) 2014 marleen berg. All rights reserved.
+//
+
+#include <stdio.h>
+void showCookTimeForTurkey(int pounds){
+    int necessaryMinutes = 15 + 15 * pounds;
+    printf("Cook turkey for %d minutes \n", necessaryMinutes);
+    if (necessaryMinutes > 120) {
+        int halfway = necessaryMinutes / 2;
+        printf("Rotate after %d of the %d minutes \n", halfway, necessaryMinutes);
+    }
+}
+int main(int argc, const char * argv[]) {
+    int totalWeight = 10;
+    int gibletsWeight = 1;
+    int turkeyWeight = totalWeight - gibletsWeight;
+    showCookTimeForTurkey(turkeyWeight);
+    return 0;
+}
