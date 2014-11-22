38bac319a31c5436a5bc756e57e79eeb24e5cdff
marleenberg1990 <marleenberg@251-85-102.wifi-owb.workstations.avans.nl>
2014-11-22 22:38:32 +0100

    Created BeerSong CLT

---
+++ ch5/BeerSong/BeerSong/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BeerSong
+//
+//  Created by Marleen Berg on 22-11-14.
+//  Copyright (c) 2014 marleen berg. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
