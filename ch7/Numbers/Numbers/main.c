39c83dd294a94d6454dd15b7f1bfb41a559fad9a
marleenberg1990 <marleenberg@251-85-102.wifi-owb.workstations.avans.nl>
2014-12-17 19:39:32 +0100

    tokens for displaying integers added

--- ch7/Numbers/Numbers/main.c
+++ ch7/Numbers/Numbers/main.c
@@ -11,6 +11,24 @@ int main(int argc, const char * argv[]) {
     
     int x = 255;
     printf("x is %d \n", x);
+    printf("In octal, x is %o \n", x);
+    printf("In hexadecimal, x is %x \n", x);
+    printf("\n");
     
+    // USE OF LONG
+    
+    long y = 255;
+    printf("y is %ld \n", y);
+    printf("In octal, y is %lo \n", y);
+    printf("In hexadecimal, y is %lx \n", y);
+    printf("\n");
+    
+    // USE OF UNSIGNED
+    
+    unsigned long z = 255;
+    printf("z is %lu \n", z);
+    // Octal and hex already assume the number was unsigned
+    printf("In octal, y is %lo \n", y);
+    printf("In hexadecimal, y is %lx \n", y);
     return 0;
 }
