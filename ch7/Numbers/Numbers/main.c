c886345126bfce3bdc6b33f485a3ca4c0dbe8962
marleenberg1990 <marleenberg@251-85-102.wifi-owb.workstations.avans.nl>
2014-12-17 19:29:37 +0100

    ch7 added

---
+++ ch7/Numbers/Numbers/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Numbers
+//
+//  Created by Marleen Berg on 17-12-14.//  Copyright (c) 2014 marleen berg. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int x = 255;
+    printf("x is %d \n", x);
+    
+    return 0;
+}
